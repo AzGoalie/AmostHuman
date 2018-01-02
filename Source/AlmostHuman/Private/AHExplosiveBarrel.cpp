0a90a72b6c27fccf9251e2177b0b79d1ebfa35d9
Travis McMahon <azgoalie@gmail.com>
2018-01-02 16:49:23 -0700

    Added networking to explosive barrels

--- Source/AlmostHuman/Private/AHExplosiveBarrel.cpp
+++ Source/AlmostHuman/Private/AHExplosiveBarrel.cpp
@@ -4,6 +4,7 @@
 #include "AHHealthComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "PhysicsEngine/RadialForceComponent.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 AAHExplosiveBarrel::AAHExplosiveBarrel()
@@ -24,6 +25,9 @@ AAHExplosiveBarrel::AAHExplosiveBarrel()
 	RadialForceComp->bIgnoreOwningActor = true;
 
 	ExplosionImpulse = 400;
+
+	SetReplicates(true);
+	SetReplicateMovement(true);
 }
 
 void AAHExplosiveBarrel::OnHealthChanged(UAHHealthComponent* OwningHealthComp, float Health, float HealthDelta, const class UDamageType* DamageType,
@@ -37,15 +41,26 @@ void AAHExplosiveBarrel::OnHealthChanged(UAHHealthComponent* OwningHealthComp, f
 	if (Health <= 0.0f)
 	{
 		bExploded = true;
+		OnRep_Exploded();
 
 		// Boost the barrel upwards
 		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
 		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
 
-		// Play FX and change material to exploded
-		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
-		MeshComp->SetMaterial(0, ExplodedMaterial);
-
 		RadialForceComp->FireImpulse();
 	}
+}
+
+void AAHExplosiveBarrel::OnRep_Exploded()
+{
+	// Play FX and change material to exploded
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+	MeshComp->SetMaterial(0, ExplodedMaterial);
+}
+
+void AAHExplosiveBarrel::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AAHExplosiveBarrel, bExploded);
 }
